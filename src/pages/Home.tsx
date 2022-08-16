cdbd13afb8847426874bdea3f45409c031754d52
Finley Sherwood <me@itsf.in>
2022-08-16 14:19:05 +1000

    Add navigation and props to pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { StatusBar } from "expo-status-bar"
+import { AppStackParamList } from "navigation/AppStack"
 import { StyleSheet, Text, View } from "react-native"
 
-export default function Home() {
+export default function Home({
+  route,
+  navigation
+}: NativeStackScreenProps<AppStackParamList, "Home">) {
   return (
     <View style={styles.container}>
       <Text>Hello world</Text>
