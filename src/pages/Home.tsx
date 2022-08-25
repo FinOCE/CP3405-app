3d6aa7d53ee2280a9d4706998c124051a2f49b71
DGMGIT <daniel.mackenzie1@my.jcu.edu.au>
2022-08-25 13:43:28 +1000

    fix image and added button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,47 +1,75 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { StatusBar } from "expo-status-bar"
 import { AppStackParamList } from "navigation/AppStack"
-import { SafeAreaView, View, FlatList, StyleSheet, Text, ListRenderItem, ListRenderItemInfo } from 'react-native';
+import {
+  SafeAreaView,
+  Image,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  ListRenderItem,
+  ListRenderItemInfo,
+  Button
+} from "react-native"
 
 interface Props {
   // any props that come into the component
-  id: string,
-  title: string,
-  description: string,
+  id: string
+  title: string
+  description: string
   usage: number
 }
 
 const DATA: Props[] = [
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First app',
-    description: 'this ia an app',
-    usage: 12,
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First app",
+    description: "this ia an app",
+    usage: 12
   },
   {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second app',
-    description: 'this ia an app',
-    usage: 11,
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second app",
+    description: "this ia an app",
+    usage: 11
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third app',
-    description: 'this ia an app',
-    usage: 10,
-  },
-];
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third app",
+    description: "this ia an app",
+    usage: 10
+  }
+]
 
 const Item = ({ title, description }: Props) => (
   <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-    <Text style={styles.title}>{description}</Text>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: "flex-start"
+      }}
+    >
+      <View style={{ width: "30%" }}>
+        <Image
+          style={styles.logo}
+          source={{
+            uri: "https://imgs.search.brave.com/eJFTgf0voEVPLCVmiAGoWPlU79tkBEPb03Rx01U_ULQ/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5k/X2llYWJxQkVOZlh6/blhjRmhmMVFRSGFI/YSZwaWQ9QXBp"
+          }}
+        />
+      </View>
+      <View style={{ width: "70%" }}>
+        '<Text style={styles.title}>{title}</Text>
+        <Text style={styles.title}>{description}</Text>
+      </View>
+    </View>
   </View>
-);
+)
 
 const App = () => {
-  const renderItem = ({ item }: ListRenderItemInfo<Props>) => (
-    <Item {...item} />  );
+  const renderItem = ({ item }: ListRenderItemInfo<Props>) => <Item {...item} />
 
   return (
     <SafeAreaView style={styles.container}>
@@ -51,7 +79,11 @@ const App = () => {
         keyExtractor={item => item.id}
       />
     </SafeAreaView>
-  );
+  )
+}
+
+const NavB = () => {
+  return
 }
 
 const styles = StyleSheet.create({
@@ -62,15 +94,19 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: "#f9c2ff",
     padding: 20,
     marginVertical: 8,
-    marginHorizontal: 16,
+    marginHorizontal: 16
   },
   title: {
-    fontSize: 30,
+    fontSize: 30
   },
-});
+  logo: {
+    width: 100,
+    height: 100
+  }
+})
 
 export default function Home({
   route,
@@ -78,8 +114,39 @@ export default function Home({
 }: NativeStackScreenProps<AppStackParamList, "Home">) {
   return (
     <View style={styles.container}>
-      <App></App>
-      <Text>Hello world</Text>
+      <View style={{ flex: 5 }}>
+        <App />
+      </View>
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            flexWrap: "wrap",
+            alignItems: "flex-start"
+          }}
+        >
+          <View style={{ width: "30%" }}>
+            <View style={{ width: 100, height: 100, backgroundColor: "red" }} />
+          </View>
+          <View style={{ width: "30%" }}>
+            <View style={{ width: 100, height: 100, backgroundColor: "blue" }}>
+              <Button
+                title="Home"
+                onPress={() => navigation.navigate("Home")}
+              />
+            </View>
+          </View>
+          <View style={{ width: "30%" }}>
+            <View style={{ width: 100, height: 100, backgroundColor: "red" }}>
+              <Button
+                title="Settings"
+                onPress={() => navigation.navigate("Settings")}
+              />
+            </View>
+          </View>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   )
