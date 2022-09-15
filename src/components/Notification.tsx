c62cde087c1e9dce4d54765c924d137456578824
Naoki-Da-Costa <naoki.dacosta@my.jcu.edu.au>
2022-09-15 13:44:01 +1000

    Create notification page

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,4 +1,5 @@
 import NotificationManager from "managers/NotificationManager"
+import { StyleSheet, Text, Button, View } from "react-native"
 
 export type NotificationProps = {
   data: Noti.Unknown
@@ -7,79 +8,118 @@ export type NotificationProps = {
 export default function Notification(props: NotificationProps) {
   // Render child request accept notifications
   if (NotificationManager.isChildRequestAccept(props.data))
-    return <div>{props.data.parent.id} accepted your request</div>
+    return (
+      <div>
+        <View style={styles.parentContainer}>
+          <View style={styles.containerLeft}>
+            <View style={styles.powderblue} />
+            <Text style={styles.contactsName}>
+              {props.data.parent.id} accepted your request
+            </Text>
+          </View>
+        </View>
+        <View style={styles.contactsPadding}></View>
+      </div>
+    )
 
   // Render child request decline notifications
   if (NotificationManager.isChildRequestDecline(props.data))
-    return <div>{props.data.parent.id} declined your request</div>
+    return (
+      <div>
+        <View style={styles.parentContainer}>
+          <View style={styles.containerLeft}>
+            <View style={styles.powderblue} />
+            <Text style={styles.contactsName}>
+              {props.data.parent.id} declined your request
+            </Text>
+          </View>
+        </View>
+        <View style={styles.contactsPadding}></View>
+      </div>
+    )
 
   // Render new app notifications
   if (NotificationManager.isNewApp(props.data))
     // TODO: Implement app notification once type created
-    return <div>New app: {props.data.app.id}</div>
+    return (
+      <div>
+        <View style={styles.parentContainer}>
+          <View style={styles.containerLeft}>
+            <View style={styles.powderblue} />
+            <Text style={styles.contactsName}>
+              You have a new app request: {props.data.app.id}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.contactsPadding}></View>
+      </div>
+    )
 
   // Render child request notifications
   if (NotificationManager.isChildRequest(props.data))
-    return <div>{props.data.child.id} requested to be your child</div>
+    return (
+      <div>
+        <View style={styles.parentContainer}>
+          <View style={styles.containerLeft}>
+            <View style={styles.powderblue} />
+            <Text style={styles.contactsName}>
+              {props.data.child.id} requested to be your child
+            </Text>
+          </View>
+        </View>
 
+        <View style={styles.parentContainer}>
+          <View style={styles.acceptContainer}>
+            <Button title="Accept" />
+          </View>
+
+          <View style={styles.declineContainer}>
+            <Button title="Decline" />
+          </View>
+        </View>
+      </div>
+    )
   return <div>Error: Unknown notification received</div>
 }
 
-/**
- * This is an example of how this can be used. You can basically copy this on
- * the actual page for now. Once you understand and implement this to the
- * notification page, feel free to remove this function since it isn't actually
- * needed. I should mention I haven't actually tested running this, but vscode
- * isn't giving me any errors and I think it should work lol. If it doesn't let
- * me know on Discord.
- */
-export function ExampleNotificationList() {
-  // Create example user to use in tests
-  const exampleUser: API.Vertex<User, "user"> = {
-    id: "userId",
-    label: "user",
-    properties: {
-      userId: [{ id: "idprop", value: "userId" }],
-      dateOfBirth: [{ id: "dobprop", value: 0 }],
-      firstName: [{ id: "firstprop", value: "First" }],
-      lastName: [{ id: "lastprop", value: "Last" }],
-      nickName: [{ id: "nickprop", value: "nick" }]
-    },
-    type: "vertex"
+const styles = StyleSheet.create({
+  acceptContainer: {
+    width: "30%",
+    marginTop: "10px",
+    marginLeft: "10%",
+    marginRight: "10%"
+  },
+  declineContainer: {
+    width: "30%",
+    marginTop: "10px",
+    marginLeft: "10%",
+    marginRight: "10%"
+  },
+  parentContainer: {
+    flexDirection: "row"
+  },
+  containerLeft: {
+    flexDirection: "row",
+    width: "75%"
+  },
+  containerRight: {
+    width: "25%"
+  },
+  contactsName: {
+    paddingLeft: 10,
+    paddingTop: 12.5
+  },
+  sendButton: {
+    fontWeight: "bold",
+    paddingTop: 12.5
+  },
+  contactsPadding: {
+    height: 15
+  },
+  powderblue: {
+    height: 50,
+    width: 50,
+    backgroundColor: "powderblue",
+    marginLeft: 10
   }
-
-  // Create examples of different types of notifications
-  const childRequestAccept: Noti.ChildRequestAccept = {
-    type: "childRequestAccept",
-    parent: exampleUser
-  }
-  const childRequestDecline: Noti.ChildRequestDecline = {
-    type: "childRequestDecline",
-    parent: exampleUser
-  }
-  const newApp: Noti.NewApp = {
-    type: "newApp",
-    app: { id: "appId", label: "app", properties: {}, type: "vertex" }
-  }
-  const childRequest: Noti.ChildRequest = {
-    type: "childRequest",
-    child: exampleUser
-  }
-
-  // Put all notifications into a single array of unknowns (since this is how the API will respond)
-  const notifications: Noti.Unknown[] = [
-    childRequestAccept,
-    childRequestDecline,
-    newApp,
-    childRequest
-  ]
-
-  // Map over all notifications and render them depending on their type
-  return (
-    <div>
-      {notifications.map(noti => (
-        <Notification data={noti} />
-      ))}
-    </div>
-  )
-}
+})
--- src/navigation/AppStack.tsx
+++ src/navigation/AppStack.tsx
@@ -1,11 +1,12 @@
 import { createStackNavigator } from "@react-navigation/stack"
 import Home from "pages/Home"
 import Settings from "pages/Settings"
-
+import Notify from "pages/Notify"
 
 export type AppStackParamList = {
   Home: undefined
   Settings: undefined
+  Notify: undefined
 }
 
 const { Navigator, Screen } = createStackNavigator<AppStackParamList>()
@@ -13,6 +14,7 @@ const { Navigator, Screen } = createStackNavigator<AppStackParamList>()
 export default function AppStack() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="Notify" component={Notify} />
       <Screen name="Home" component={Home} />
       <Screen name="Settings" component={Settings} />
     </Navigator>
